76693d71a81490c6b79d4d7f0d47975aa0010d0b
Somfic <somfic@gmail.com>
2020-12-22 14:24:10 +0100

    1.0.6

--- src/main.js
+++ src/main.js
@@ -9,11 +9,12 @@ module.exports = () =>
     new Promise(async (resolve, reject) => {
         try {
             let solution = core.getInput("solution");
-            //let solution = "C:\\Users\\somfi\\Documents\\GitHub\\EliteAPI\\EliteAPI.sln";
+            // let solution =
+            //     "C:\\Users\\somfi\\Documents\\GitHub\\EliteAPI\\EliteAPI.sln";
             await prepare(config.temp);
             await download(config.resharperDownload, config.tempFile);
             await decompress(config.tempFile, config.tempDirectory);
-            await command(`cleanupcode ${solution}`);
+            await command(`./cleanupcode.sh ${solution}`);
             resolve();
         } catch (err) {
             reject(err);
